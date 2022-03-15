83349bf38304ac6b832a174ea94ca9b09841bf1d
constantinoi <pescaalma@gmail.com>
2022-03-14 21:17:29 -0300

    configurando persistence

---
+++ src/main/java/util/HibernateUtil.java
@@ -0,0 +1,29 @@
+package util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+	public static EntityManagerFactory emf = null;
+
+	static {
+		init();
+	}
+
+	private static void init() {
+		try {
+
+			if (emf == null) {
+				emf = Persistence.createEntityManagerFactory("pos-java-hibernate");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();/* Prover a parte de persistencia */
+	}
+}
---
+++ src/test/java/util/TesteHibernate.java
@@ -0,0 +1,11 @@
+package util;
+
+import org.junit.Test;
+
+public class TesteHibernate {
+
+	@Test
+	public void testeHibernateUtil() {
+		HibernateUtil.getEntityManager();
+	}
+}
