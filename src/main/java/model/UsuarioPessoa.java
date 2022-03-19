8559cf0739220e277062a959c3851e0738ea37bd
constantinoi <pescaalma@gmail.com>
2022-03-18 22:07:08 -0300

    Relacionamento

--- src/main/java/model/UsuarioPessoa.java
+++ src/main/java/model/UsuarioPessoa.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 @Entity
 @NamedQueries({ @NamedQuery(name = "UsuarioPessoa.findAll", query = "SELECT u FROM UsuarioPessoa u"),
@@ -22,6 +25,9 @@ public class UsuarioPessoa {
 	private String login;
 	private String senha;
 	private Long idade;
+
+	@OneToMany(mappedBy = "usuarioPessoa")
+	private List<TelefoneUser> listaTelefone;
 
 	public Long getId() {
 		return id;
@@ -77,6 +83,14 @@ public class UsuarioPessoa {
 
 	public void setIdade(Long idade) {
 		this.idade = idade;
+	}
+
+	public List<TelefoneUser> getListaTelefone() {
+		return listaTelefone;
+	}
+
+	public void setListaTelefone(List<TelefoneUser> listaTelefone) {
+		this.listaTelefone = listaTelefone;
 	}
 
 	@Override
